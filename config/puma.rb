3314956446a96dea55499f60e9bbf0346014e1f1
Truong Hoang Dung <checkraiser11@gmail.com>
2016-01-26 16:07:15 +0700

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,5 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-  SuckerPunch.config do
-    queue name: :refresh_matview_queue, worker: RefreshMatviewJob, workers:2
-  end
+  
 end
